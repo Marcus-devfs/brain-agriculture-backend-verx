8301aa2fd9e02ba10b26f62d9b95c29c4e992316
Marcus-devfs <marcusvini6277@gmail.com>
2025-06-09 15:22:33 -0300

    config cors

--- src/app.ts
+++ src/app.ts
@@ -4,8 +4,15 @@ import propostaRoutes from './routes/proposalRoutes';
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: 'https://seu-front.vercel.app', // substitua pelo domínio correto do frontend na Vercel
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+}));
+app.options('*', cors());
 app.use(express.json());
 app.use('/', propostaRoutes);
+
 
 export default app;
